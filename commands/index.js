e754debcf4358935122a1ab37d8f922693ce4592
most-trusted <hapandiohome@gmail.com>
2025-10-28 17:20:10 +0300

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,10 @@
+import ping from './ping.js';
+import ai from './ai.js';
+import sticker from './sticker.js';
+import tagall from './tagall.js';
+import autotyping from './autotyping.js';
+import alwaysonline from './alwaysonline.js';
+import autoview from './autoview.js';
+
+export const commands = [ping, ai, sticker, tagall, autotyping, alwaysonline, autoview];
+  
